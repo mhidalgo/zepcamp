0cae6d9980e0bbcdfea2d9f5131f887a2a67da5e
Marvin Hidalgo <marvin.hidalgo@gmail.com>
2008-11-16 01:11:40 -0500

    Moved the authentication command to application file

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,6 +4,9 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
+  include AuthenticatedSystem
+  
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '875bb8629bace8cda58c39d5a65bfd59'
